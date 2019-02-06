e91d3a1461156360dd690830e89f77d3170b4db1
kam <kmathes348@students.mchenry.edu>
2019-02-06 16:44:17 -0600

    final

--- app/src/main/java/mad155/kmathes/mad155module3concerttickets/MainActivity.java
+++ app/src/main/java/mad155/kmathes/mad155module3concerttickets/MainActivity.java
@@ -55,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
                     results.setText("Cost for group - " + groupChoice + " is: " +
                             currency.format(totalCost));
                 }
-
             }
         });
 
